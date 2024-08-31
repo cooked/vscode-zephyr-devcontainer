8e921d9cb50cd8b9603b142565a89d85dacf5369
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2024-08-31 18:33:32 +0200

    WIP mostly debug working (not dynamic)

---
+++ .vscode/c_cpp_undef.h
@@ -0,0 +1,12 @@
+#ifndef C_CPP_UNDEF_H__
+#define C_CPP_UNDEF_H__
+
+#undef __x86_64
+#undef __i386
+#undef __APPLE__
+#undef __unix__
+#undef __unix
+#undef unix
+#undef _WIN32
+
+#endif /* C_CPP_UNDEF_H__ */
